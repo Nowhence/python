69420f8f287852f4fb3474f99092986e3815bb22
Nowhence <129984317+Nowhence@users.noreply.github.com>
2023-05-19 11:48:28 -0300

    Add files via upload

---
+++ print.py
@@ -0,0 +1,32 @@
+print("miguel")
+
+print("zion")
+
+print('fulano'"é meu amigo") 
+
+print("zion \n é meu\" amigo2\"")
+
+print("1 x 1 =" ,1*1)
+
+print("10² =" ,10**3)
+
+print("10² =",pow(10,30))
+
+nome =""
+print(nome)
+nome = 1
+print(nome)
+nome = True
+print(nome)
+
+nome = "A"
+x = 1
+x = 1+1
+y = True
+
+print(x)
+
+D1 = 5.6
+D2 = "5"
+print(int(D1 + int(D2)))
+print(int(D1 + int(D2)))
